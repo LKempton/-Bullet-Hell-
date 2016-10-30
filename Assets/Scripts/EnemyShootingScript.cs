ea2d45235082d843dad3577005c5121b9813b3f3
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2016-10-30 23:51:14 +0000

    Added the first enemy, which spawns bullets in a line

---
+++ Assets/Scripts/EnemyShootingScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShootingScript : MonoBehaviour {
+
+    public Transform[] spawners = new Transform[5];
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private float repeatRate = 2f;
+
+    void Start()
+    {
+        InvokeRepeating("Shoot", 3f, repeatRate);
+    }
+
+    private void Shoot()
+    {
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            Instantiate(bullet, spawners[i].position, spawners[i].rotation);
+        }
+    }
+}
